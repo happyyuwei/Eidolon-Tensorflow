54371d685ebc6de3884f8f41def6fe29fba36767
happyyuwei1994 <happyyuwei1994@qq.com>
2020-02-19 13:03:36 +0800

    1.9.2.2

--- hide/secret_container.py
+++ hide/secret_container.py
@@ -35,13 +35,13 @@ class SecretContainer(train.Container):
         self.on_prepare_dataset()
 
         # 加载秘密图像数据集
-        temp=self.config_loader.image_type
-        self.config_loader.image_type="png"
+        temp = self.config_loader.image_type
+        self.config_loader.image_type = "png"
         secret_loader = loader.ImageLoader("../../hide/img/", is_training=True)
         self.secret_dataset = secret_loader.load(
             self.config_loader, load_function=secret_load.load_secret_image)
-        self.config_loader.image_type=temp
-        
+        self.config_loader.image_type = temp
+
         # 创建编码网络
         self.encoder = UNet(input_shape=[self.config_loader.image_width, self.config_loader.image_height, 6],
                             high_performance_enable=self.config_loader.high_performance)
@@ -72,8 +72,12 @@ class SecretContainer(train.Container):
 
     def compute_loss(self, input_image, target, secret_image):
 
-        #输入合并
-        input_tensor=tf.concat([input_image, secret_image], axis=-1)
+        # 任务网络
+        task_output = self.decoder(input_image, training=True)
+        task_loss = loss_util.pixel_loss(task_output, target)
+
+        # 输入合并
+        input_tensor = tf.concat([input_image, secret_image], axis=-1)
 
         # 计算生成网络输出图像
         gen_output = self.encoder(input_tensor, training=True)
@@ -86,13 +90,14 @@ class SecretContainer(train.Container):
 
         decoder_loss = loss_util.pixel_loss(secret, secret_image)
 
-        total_loss = encoder_loss+decoder_loss
+        total_loss = encoder_loss+decoder_loss+task_loss
 
         # 合并结果集
         loss_set = {}
         loss_set["total_loss"] = total_loss
         loss_set["encoder_loss"] = encoder_loss
         loss_set["decoder_loss"] = decoder_loss
+        loss_set["task_loss"]=task_loss
 
         # 记录损失和输出
         result_set = {
@@ -113,10 +118,10 @@ class SecretContainer(train.Container):
         """
         训练一批数据，该函数允许使用tensorflow加速
         """
-        secret_image=None
+        secret_image = None
         for each in self.secret_dataset.take(1):
-            secret_image=each
-        
+            secret_image = each
+
         # 创建梯度计算器，负责计算损失函数当前梯度
         with tf.GradientTape() as gen_tape:
 
@@ -152,8 +157,8 @@ class SecretContainer(train.Container):
         for _, (test_input, test_target) in self.test_dataset.enumerate():
 
             for secret_image in self.secret_dataset.take(1):
-                #输入合并
-                input_tensor=tf.concat([test_input, secret_image], axis=-1)
+                # 输入合并
+                input_tensor = tf.concat([test_input, secret_image], axis=-1)
                 # 生成测试结果
                 predicted_image = self.encoder(input_tensor, training=True)
                 result_set = eval_util.evaluate(predicted_image, test_input)
@@ -181,8 +186,12 @@ class SecretContainer(train.Container):
         for test_input, test_target in self.test_dataset.take(1):
 
             for secret_image in self.secret_dataset.take(1):
-                #输入合并
-                input_tensor=tf.concat([test_input, secret_image], axis=-1)
+                
+                #task
+                task_output = self.decoder(test_input, training=True)
+
+                # 输入合并
+                input_tensor = tf.concat([test_input, secret_image], axis=-1)
 
                 # 生成测试结果
                 predicted_image = self.encoder(input_tensor, training=True)
@@ -191,8 +200,8 @@ class SecretContainer(train.Container):
                 secret = self.decoder(predicted_image, training=True)
 
         # 排成列表
-        image_list = [test_input,predicted_image, secret_image, secret]
-        title_list = ["C", "CS", "GT", "PR"]
+        image_list = [test_input, predicted_image, secret_image, secret, task_output, test_target]
+        title_list = ["C", "CS", "S_GT", "S_PR", "T_PR", "T_GT"]
         return image_list, title_list
 
     def on_test_epoch(self, current_epoch, loss_set):
